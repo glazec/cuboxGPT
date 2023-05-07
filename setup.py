7b4d53fb9ddd74e8f0dd763b9f809aba8e9dc2c1
glaze <i@inevitable.tech>
2023-05-07 13:26:50 -0400

    update pypi configuration

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
+import os
+# Read the contents of the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='cuboxGPT',
-    version='0.1.0',
+    version='0.1.1',
+    author='Glaze',
+    description='Use GPT to chat/search your large Cubox datasets',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    readme='README.md',
+    keywords=['Cubox', 'search', 'AI', 'GPT', 'langchain'],
+    url='https://github.com/glazec/cuboxGPT',
     py_modules=['webParser', 'db', 'chatFromDB', 'cuboxGPT'],
     install_requires=[
         'typer',
@@ -19,4 +31,16 @@ setup(
             "cuboxGPT=cuboxGPT:app",
         ],
     },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+
+    ],
 )
